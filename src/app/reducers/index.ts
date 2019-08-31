71bc514e33b68458873d0686484ab5c0e52af10a
Noushadpp <to.noushadpp@gmail.com>
2019-08-31 14:23:11 +0200

    Remove notification feature, Update test case

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,13 +1,11 @@
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
 import { combineReducers } from 'redux';
-import { notificationsReducer, STORE_KEY_NOTIFICATIONS } from '../../features/notifications';
 import { ApplicationState } from '../types';
 
 const createRootReducer = (history: History) =>
   combineReducers<ApplicationState>({
     router: connectRouter(history),
-    [STORE_KEY_NOTIFICATIONS]: notificationsReducer,
   });
 
 export default createRootReducer;
--- src/app/sagas/index.ts
+++ src/app/sagas/index.ts
@@ -1,10 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import apiRequestSaga from './apiRequest';
-import notificationsSaga from './notifications';
 
 function* rootSaga() {
   yield fork(apiRequestSaga);
-  yield fork(notificationsSaga);
 }
 
 export default rootSaga;
